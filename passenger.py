48571cf73ddfd8b5a4b3d64e33efb6df9559d736
Brittany Harrison <bkharrison94@gmail.com>
2021-07-06 11:50:33 +0100

    passenger database added

--- passenger.py
+++ passenger.py
@@ -1,21 +1,32 @@
 import sqlite3
+import datetime
 from person import Person
 
-conn = sqlite3.connect('databases/staff.db')
+conn = sqlite3.connect('databases/passengers.db')
 c = conn.cursor()
 
 c.execute("""
-CREATE TABLE passenger(
-) """)
+CREATE TABLE passengers(
+passport_id integer,
+first_name text,
+last_name text,
+dob text)""")
+
 
 class Passengers(Person):
-    def __init__(self, passport_id, flight_id,first_namegit a ,):
-        super().__init__()
+    def __init__(self, passport_id, first_name, last_name, dob):
+        super().__init__(first_name, last_name, dob)
         self.passport_id = passport_id
-        self.flight_id = flight_id
 
     def add_passenger(self):
-        pass
+        query = "INSERT into passengers VALUES (:passport_id,:first_name,:last_name,:dob)"
+        with conn:
+            c.execute(query, {
+                'passport_id': self.passport_id,
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'dob': self.dob
+            })
 
     def book_flight_trip(self):
         pass
@@ -23,5 +34,11 @@ class Passengers(Person):
     def view_booking(self):
         pass
 
-    def cancel
 
+pass1 = Passengers(31195344, 'Jacob', 'Sinclair', datetime.datetime(1994, 10, 21))
+pass2 = Passengers(83636482, 'Harry', 'Smith', datetime.datetime(2014, 4, 2))
+pass3 = Passengers(31195344, 'Karen', 'Fraser', datetime.datetime(2020, 12, 12))
+
+pass1.add_passenger()
+pass2.add_passenger()
+pass3.add_passenger()
