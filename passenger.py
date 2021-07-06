5ef5c2a6b216e0ba5b8b9c33a5743305ffeed8ed
Brittany Harrison <bkharrison94@gmail.com>
2021-07-06 14:03:40 +0100

    updated methods

--- passenger.py
+++ passenger.py
@@ -1,44 +1,50 @@
-import sqlite3
 import datetime
+import sqlite3
 from person import Person
+from datetime import date
 
 conn = sqlite3.connect('databases/passengers.db')
 c = conn.cursor()
 
-c.execute("""
-CREATE TABLE passengers(
-passport_id integer,
-first_name text,
-last_name text,
-dob text)""")
-
-
 class Passengers(Person):
-    def __init__(self, passport_id, first_name, last_name, dob):
-        super().__init__(first_name, last_name, dob)
-        self.passport_id = passport_id
 
-    def add_passenger(self):
-        query = "INSERT into passengers VALUES (:passport_id,:first_name,:last_name,:dob)"
+
+    def create_passenger_list(flight_id):
+        query = f"""
+        CREATE TABLE IF NOT EXISTS {flight_id}(
+        passport_id integer, 
+        first_name text,
+        last_name text,
+        dob text)"""
+        with conn:
+            return c.execute(query)
+
+    def add_passenger_to_list(flight_id, passport_id, first_name, last_name, dob):
+        query = f"INSERT into {flight_id} VALUES (:passport_id,:first_name,:last_name,:dob)"
         with conn:
             c.execute(query, {
-                'passport_id': self.passport_id,
-                'first_name': self.first_name,
-                'last_name': self.last_name,
-                'dob': self.dob
+                'passport_id': passport_id,
+                'first_name': first_name,
+                'last_name': last_name,
+                'dob': dob
             })
 
-    def book_flight_trip(self):
-        pass
+    def view_passengers_list(flight_id):
+        query = f"SELECT * FROM {flight_id}"
+        with conn:
+            c.execute(query)
+            return c.fetchall()
 
-    def view_booking(self):
-        pass
+    def remove_passenger(flight_id, passport_id):
+        query = f"DELETE FROM {flight_id} WHERE passenger_id = :passenger_id"
+        with conn:
+            return c.execute(query, {'passport_id': passport_id})
+
+    def calculate_age(dob):
+        today = date.today()
+        return today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
 
 
-pass1 = Passengers(31195344, 'Jacob', 'Sinclair', datetime.datetime(1994, 10, 21))
-pass2 = Passengers(83636482, 'Harry', 'Smith', datetime.datetime(2014, 4, 2))
-pass3 = Passengers(31195344, 'Karen', 'Fraser', datetime.datetime(2020, 12, 12))
 
-pass1.add_passenger()
-pass2.add_passenger()
-pass3.add_passenger()
+
+
