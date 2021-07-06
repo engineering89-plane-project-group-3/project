8c492d7613c8c61093724d708496e6f42ce9c8ce
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-07-06 17:12:17 +0100

    File cleanup
    Co-Authored-By: Ahhhh-man <85831666+Ahhhh-man@users.noreply.github.com>
    Co-Authored-By: SBenkhelfaSparta <86235740+SBenkhelfaSparta@users.noreply.github.com>
    Co-Authored-By: Brittany Harrison <84725608+brittanyharrison@users.noreply.github.com>
    Co-Authored-By: RayWLMo <86238266+RayWLMo@users.noreply.github.com>

--- app/person.py
+++ app/person.py
@@ -1,7 +1,5 @@
-import datetime
-
 class Person:
     def __init__(self, first_name, last_name, dob):
         self.first_name = first_name
         self.last_name = last_name
-        self.dob = datetime.datetime(dob)
+        self.dob = dob
