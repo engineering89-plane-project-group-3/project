e42b1dbfe87c72920e304390acfd281f6f6a72b8
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-07-06 15:45:36 +0100

    Completed stuff for flight_trip for testing

--- flight_trip.py
+++ flight_trip.py
@@ -1,36 +1,42 @@
 import sqlite3
+import datetime
 
+flight_trip_db = sqlite3.connect('databases/flight_trip.db')
+flight_trip_db_cursor = flight_trip_db.cursor()
 
-class FlightTrip:
+destinations_db = sqlite3.connect('databases/destinations.db')
+destinations_db_cursor = destinations_db.cursor()
 
-    passenger_list = []
+def database_initialise():
+    flight_trip_db_cursor.execute("""CREATE TABLE IF NOT EXISTS flight_trip (
+        flight_id text,
+        aircraft_id text,
+        capacity integer,
+        destination text,
+        departure_time text,
+        arrival_time text,
+        terminal_id text,
+        price real
+    )""")
 
-    def __init__(self, flight_id, aircraft_id, destination, departure_time, arrival_time,terminal_id,price):
-        self.flight_id = flight_id
-        self.aircraft_id = aircraft_id
-        self.destination = destination
-        self.departure_time = departure_time
-        self.arrival_time = arrival_time
-        self.price = price
+def create_flight_trip(flight_id, aircraft_id, capacity, destination, departure_time, arrival_time, terminal_id):
+    destinations_db_cursor.execute("SELECT distance FROM destinations WHERE destination = (?)", (destination,))
+    distance = destinations_db_cursor.fetchone()[0]
+    price = 0.10 * distance
 
-    def create_flight_trip(self):
-        pass
+    flight_trip_db_cursor.execute("INSERT INTO flight_trip VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (
+    flight_id, aircraft_id, capacity, destination, departure_time, arrival_time, terminal_id, price))
+    flight_trip_db.commit()
 
-    def add_passenger_to_list(self):
-        pass
+def change_flight_trip():
+    pass
 
-    def remove_passenger_from_list(self):
-        pass
+def available_flights():
+    flight_trip_db_cursor.execute("SELECT flight_id, destination from flight_trip")
+    return flight_trip_db_cursor.fetchall()
 
-    def view_passenger_list(self):
-        pass
+def view_flight_trip(flight_id):
+    flight_trip_db_cursor.execute("SELECT * FROM flight_trip WHERE flight_id = (?)", (flight_id,))
+    return flight_trip_db_cursor.fetchall()
 
-    def change_flight_trip(self):
-        pass
-
-    def view_flight_trip(self):
-        pass
-
-    def calculate_price(self, age, destination):
-        pass
-
+create_flight_trip("BA_003", "3", "200", "Athens", "14:00", "17:45", "B")
